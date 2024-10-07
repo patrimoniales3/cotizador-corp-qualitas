6fb2855ce2f3054d60d14452d29488308aa2b054
patrimoniales3 <patrimoniales3@gfconsultor.com>
2024-10-07 14:47:25 -0500

    Crear Pagina git

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    assetPrefix: '/cotizador-corp-qualitas/',
+    basePath: '/cotizador-corp-qualitas',
+    images: {
+      unoptimized: true,
+    },
+    reactStrictMode: true,
+  }
+  
+  export default nextConfig;
