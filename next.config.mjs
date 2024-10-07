5023fafb65f90d830daf14bc649b26be570a2cc7
patrimoniales3 <patrimoniales3@gfconsultor.com>
2024-10-07 14:51:29 -0500

    Regreso a version anterior

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    assetPrefix: '/cotizador-corp-qualitas/',
-    basePath: '/cotizador-corp-qualitas',
-    images: {
-      unoptimized: true,
-    },
-    reactStrictMode: true,
-  }
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
